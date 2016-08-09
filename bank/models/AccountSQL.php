e236c945c5a8216f18ad59b29aa1060b6a8a5408
betsy_liu <40143204@gm.nfu.edu.tw>
2016-08-09 08:38:34 +0000

    銀行:AccountSQL.php修改排版

--- bank/models/AccountSQL.php
+++ bank/models/AccountSQL.php
@@ -1,30 +1,32 @@
 <?php
 
-    require_once("Database.php");
-    
-    class AccountSQL extends Database
+require_once("Database.php");
+
+class AccountSQL extends Database
+{
+    function selectAccount()
     {
-        function selectAccount()
-        {
-            $sql_select_account = " SELECT `user`,`balance`
-                                    FROM `account`";
-            $row_select_account = $this->select($sql_select_account);
-            return $row_select_account;
-        }
-    
-        function selectBalance($user)
-        {
-            $sql_select_balance = " SELECT `user`,`balance`
-                                    FROM `account`
-                                    WHERE `user`='$user'";
-            $row_select_balance = $this->select($sql_select_balance);
-            return $row_select_balance;
-        }
-    
-        function updateBalance($user, $balance_new)
-        {
-            $sql_update_balance = " UPDATE `account` SET `balance`='$balance_new'
-                                    WHERE `user`= '$user'";    
-            $row_update_balance = $this->update($sql_update_balance);
-        }
+        $sql_select_account = "SELECT `user`,`balance`
+                               FROM `account`";
+        $row_select_account = $this->select($sql_select_account);
+
+        return $row_select_account;
     }
+
+    function selectBalance($user)
+    {
+        $sql_select_balance = "SELECT `user`,`balance`
+                               FROM `account`
+                               WHERE `user` = '$user'";
+        $row_select_balance = $this->select($sql_select_balance);
+
+        return $row_select_balance;
+    }
+
+    function updateBalance($user, $balance_new)
+    {
+        $sql_update_balance = "UPDATE `account` SET `balance` = '$balance_new'
+                               WHERE `user` = '$user'";
+        $row_update_balance = $this->update($sql_update_balance);
+    }
+}
