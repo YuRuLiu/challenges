0e563f13dd99b0855d3b44538841bdf1ee9c165c
betsy_liu <40143204@gm.nfu.edu.tw>
2016-08-09 08:28:10 +0000

    銀行:App.php修改排版

--- bank/core/App.php
+++ bank/core/App.php
@@ -1,34 +1,45 @@
 <?php
 
-class App {
-    
-    public function __construct() 
+class App
+{
+
+    public function __construct()
     {
         $url = $this->parseUrl();
-        
+
         $controllerName = "{$url[0]}Controller";
-        if (!file_exists("controllers/$controllerName.php"))
+
+        if (!file_exists("controllers/$controllerName.php")) {
+
             return;
+        }
+
         require_once "controllers/$controllerName.php";
         $controller = new $controllerName;
         $methodName = isset($url[1]) ? $url[1] : "index";
-        if (!method_exists($controller, $methodName))
+
+        if (!method_exists($controller, $methodName)) {
+
             return;
-        unset($url[0]); unset($url[1]);
+        }
+
+        unset($url[0]);
+        unset($url[1]);
         $params = $url ? array_values($url) : Array();
         call_user_func_array(Array($controller, $methodName), $params);
     }
-    //$url[0]是controllerName,$url[1]是methodName
-    public function parseUrl() 
+
+    public function parseUrl()
     {
-        if (isset($_GET["url"])) 
-        {
+
+        if (isset($_GET["url"])) {
             $url = rtrim($_GET["url"], "/");
             $url = explode("/", $url);
+
             return $url;
-        }
-        else 
+        } else {
             header("location:/challenges/bank/Account/displayAccount");
+        }
+
     }
-    
 }
