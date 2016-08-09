f218f5f7d6d6385335e29251b19598c806b1bdbc
betsy_liu <40143204@gm.nfu.edu.tw>
2016-08-09 08:32:42 +0000

    銀行:Controller.php修改排版

--- bank/core/Controller.php
+++ bank/core/Controller.php
@@ -1,13 +1,15 @@
 <?php
 
-class Controller {
-    public function model($model) 
+class Controller
+{
+    public function model($model)
     {
         require_once "../bank/models/$model.php";
+
         return new $model ();
     }
-    
-    public function view($view, $data1 = Array(),$data2 = Array(),$data3 = Array(),$data4 = Array(),$data5 = Array(),$data6 = Array(),$data7 = Array(),$data8 = Array(),$data9 = Array(),$data10 = Array(),$data11 = Array()) 
+
+    public function view($view, $data1 = Array(), $data2 = Array(), $data3 = Array(), $data4 = Array(), $data5 = Array(), $data6 = Array(), $data7 = Array(), $data8 = Array(), $data9 = Array(), $data10 = Array(), $data11 = Array())
     {
         require_once "../bank/views/$view.php";
     }
